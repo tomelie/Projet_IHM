e1d522cfe4e544ca30e1fb83d4316b7c4358044e
Laurent <soyris@gmail.com>
2018-10-10 15:45:00 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import {TmdbService} from './tmdb.service';
+import {MovieResponse} from './tmdb-data/Movie';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {auth, User} from 'firebase';
+import {Observable} from 'rxjs';
+import {AngularFireDatabase} from '@angular/fire/database';
+import {filter} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  private _movie: MovieResponse;
+  private _user: User;
+  private dbData: Observable<any>;
+
+  constructor(private tmdb: TmdbService, public anAuth: AngularFireAuth, private db: AngularFireDatabase) {
+    this.anAuth.user.pipe(filter( u => !!u )).subscribe( u => {
+      this._user = u;
+      const listsPath = `lists/${u.uid}`;
+      const lists = db.list(listsPath);
+      lists.push('coucou');
+      this.dbData = lists.valueChanges();
+    });
+    setTimeout( () =>
+      tmdb.init('XXX') // Clef de TMDB
+          .getMovie(13)
+          .then( (m: MovieResponse) => console.log('Movie 13:', this._movie = m) )
+          .catch( err => console.error('Error getting movie:', err) ),
+      1000 );
+
+  }
+
+  get movie(): MovieResponse {
+    return this._movie;
+  }
+
+  getPath(path: string): string {
+    return `https://image.tmdb.org/t/p/w500${path}`;
+  }
+
+  login() {
+    this.anAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.anAuth.auth.signOut();
+    this._user = undefined;
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get lists(): Observable<any> {
+    return this.dbData;
+  }
+}
+// /yE5d3BUhE8hCnkMUJOo1QDoOGNz.jpg
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {TmdbService} from './tmdb.service';
+import {HttpClientModule} from '@angular/common/http';
+import {AngularFireModule} from '@angular/fire';
+import {environment} from '../environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp( environment.firebase ),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
+  ],
+  providers: [TmdbService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
