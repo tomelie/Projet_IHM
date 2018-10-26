ac69f78592d388115956ba19cdf3ba7cd18d6ecc
tomelie <tomelie@protonmail.com>
2018-10-26 20:02:21 +0200

    Création des service : Popular movie Latest Movie Genre Movie

--- src/app/tmdb.service.ts
+++ src/app/tmdb.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {MovieQuery, MovieResponse} from './tmdb-data/Movie';
+import {MovieGenre, MovieQuery, MovieResponse} from './tmdb-data/Movie';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {PersonQuery, PersonResponse} from './tmdb-data/Person';
 import {SearchMovieQuery, SearchMovieResponse} from './tmdb-data/searchMovie';
@@ -81,6 +81,33 @@ export class TmdbService {
   async searchTV(query: SearchTVQuery): Promise<SearchTVResponse> {
     const url = `${tmdbApi}/search/tv`;
     const res = await this.get<SearchTVResponse>(url, query);
+    return res.body;
+  }
+
+  // _______________________________________________________________________________________________________________________________________
+  // Popular Movie ____________________________________________________________________________________________________________________________________
+  // _______________________________________________________________________________________________________________________________________
+  async getPopularMovie(options?: MovieQuery): Promise<MovieResponse> {
+    const url = `${tmdbApi}/movie/popular`;
+    const res = await this.get<MovieResponse>(url, options);
+    return res.body;
+  }
+
+  // _______________________________________________________________________________________________________________________________________
+  // Latest Movie ____________________________________________________________________________________________________________________________________
+  // _______________________________________________________________________________________________________________________________________
+  async getLatestMovie(options?: MovieQuery): Promise<MovieResponse> {
+    const url = `${tmdbApi}/movie/latest`;
+    const res = await this.get<MovieResponse>(url, options);
+    return res.body;
+  }
+
+  // _______________________________________________________________________________________________________________________________________
+  // Genre Movie ____________________________________________________________________________________________________________________________________
+  // _______________________________________________________________________________________________________________________________________
+  async getListGenreMovie(options?: MovieQuery): Promise<MovieGenre> {
+    const url = `${tmdbApi}/genre/movie/list`;
+    const res = await this.get<MovieGenre>(url, options);
     return res.body;
   }
 
