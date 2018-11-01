8720dff16fb1fb4038f2c227d4f2812d2aad22e2
masselit <thibaut.masselin@free.fr>
2018-11-01 22:51:49 +0100

    routing

--- src/app/listmovie/listmovie.component.ts
+++ src/app/listmovie/listmovie.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TmdbService} from '../tmdb.service';
 import {MovieResponse} from '../tmdb-data/Movie';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-listmovie',
@@ -13,10 +14,19 @@ export class ListmovieComponent implements OnInit {
 
   @Input() movieId: number;
 
-  constructor(private tmdb: TmdbService) { }
+  constructor(private tmdb: TmdbService, private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      if(params.type === "action"){
+        console.log('test OK');
+      }
+      console.log(params);
+      //Action select option
+    });
+   }
 
   ngOnInit() {
-    this.tmdb.getMovie(this.movieId).then(data => this.data = data);
+
+    //this.tmdb.getMovie(this.movieId).then(data => this.data = data);
   }
 
 }
