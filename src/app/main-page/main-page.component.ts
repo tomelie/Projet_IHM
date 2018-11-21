b94e141d7a1c7347531b74878fc5d877a86dc71e
Thibaut Masselin <masselit@F211-11.e-im2ag.ujf-grenoble.fr>
2018-11-21 16:48:37 +0100

    redirect home ->  tendances

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-main-page',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.router.navigate(['/movies/tendances']);
+  }
 
   ngOnInit() {
   }
